3d3538eaadcab35171160dd3f67db9477c4334dc
Mario Godin <victor@sheet2scene.ca>
2025-11-21 14:12:02 -0500

    Fix AAA v2 ERA imports

--- apps/web/lib/s2s/engine/index.ts
+++ apps/web/lib/s2s/engine/index.ts
@@ -1,13 +1,6 @@
-// Ré-exports publics du Mithril Engine AAA v2
+// Ré-exports publics du Mithril Engine
 
-// Worlds / Univers
-export { default as getWorlds } from "@engineContent/worlds";
-
-// Eras (époques) pour chaque univers
-export { default as getErasForWorld } from "@engineContent/eras";
-
-// Bestiaire
+export { default as getWorlds } from "@engine/getWorlds";
+export { default as getErasForWorld } from "@engine/getWorlds";
 export { default as bestiary } from "@engineContent/bestiary";
-
-// Sorts / compétences
 export { default as spells } from "@engineContent/spells";
