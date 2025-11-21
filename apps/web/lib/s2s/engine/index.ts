e2d087613637d0d30b635174ebb2fffab95a7b1c
Mario Godin <victor@sheet2scene.ca>
2025-11-21 14:04:44 -0500

    Fix AAA v2 engineContent imports

--- apps/web/lib/s2s/engine/index.ts
+++ apps/web/lib/s2s/engine/index.ts
@@ -1,12 +1,13 @@
-// Ré-exports publics du Mithril Engine
+// Ré-exports publics du Mithril Engine AAA v2
 
-export { default as getWorlds } from "../fantasy/worlds";
-export { default as getErasForWorld } from "../fantasy/eras";
-export * as bestiary from "../fantasy/bestiary";
-export * as spells from "../fantasy/spells";
-export * as items from "../fantasy/items";
+// Worlds / Univers
+export { default as getWorlds } from "@engineContent/worlds";
 
-// Exports complilés
-export * as core from "./dist/gme/core";
-export * as gme from "./dist/gme/index";
-export * as gmeTypes from "./dist/gme/types";
+// Eras (époques) pour chaque univers
+export { default as getErasForWorld } from "@engineContent/eras";
+
+// Bestiaire
+export { default as bestiary } from "@engineContent/bestiary";
+
+// Sorts / compétences
+export { default as spells } from "@engineContent/spells";
