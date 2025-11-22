6129b8e386a440e2366e4b2c26c6b3e9fa037695
Mario Godin <victor@sheet2scene.ca>
2025-11-21 22:36:30 -0500

    Fix getWorlds imports (AAA clean)

--- packages/engine/src/getWorlds.ts
+++ packages/engine/src/getWorlds.ts
@@ -1,13 +1,11 @@
 import { WORLDS } from "./content/fantasy/worlds";
 import { ERAS_BY_WORLD } from "./content/fantasy/eras";
-import type { EraDefinition, WorldDefinition } from "./content/types/worlds";
 
-export function getWorlds(): WorldDefinition[] {
+export function getWorlds() {
   return WORLDS;
 }
 
-export function getErasForWorld(worldId: string): EraDefinition[] {
-  return (ERAS_BY_WORLD as Record<string, EraDefinition[]>)[worldId] ?? [];
+export function getErasForWorld(worldId: string) {
+  return ERAS_BY_WORLD[worldId] ?? [];
 }
-
 export default WORLDS;
