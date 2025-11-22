d9b24cbc3e71e7f28b45521cff1b7ce70c9fb5cc
Mario Godin <victor@sheet2scene.ca>
2025-11-21 21:55:24 -0500

    Fix bugengine

--- packages/engine/src/getWorlds.ts
+++ packages/engine/src/getWorlds.ts
@@ -1,12 +1,12 @@
-import { WORLDS } from "./content/fantasy/worlds";
-import { ERAS_BY_WORLD } from "./content/fantasy/eras";
+import { WORLDS } from "./content/fantasy/worlds.ts";
+import { ERAS_BY_WORLD } from "./content/fantasy/eras/index.ts";
 
 // Liste complète des mondes
 export function getWorlds() {
   return WORLDS;
 }
 
-// Retourne la liste des ères pour un monde donné
+// Liste des ères d’un monde donné
 export function getErasForWorld(worldId: string) {
   return ERAS_BY_WORLD[worldId] ?? [];
 }
