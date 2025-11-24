ecb25895cd9728d88846a2dde968a5074046f55c
Mario Godin <victor@sheet2scene.ca>
2025-11-23 20:12:26 -0500

    fix

--- apps/web/app/grimoire/[id]/page.tsx
+++ apps/web/app/grimoire/[id]/page.tsx
@@ -4,7 +4,7 @@ import { promises as fs } from "fs";
 import { BookShell, PageLeft, PageRight } from "@components/BookShell";
 import FlipLink from "@components/FlipLink";
 import EditQuick from "./EditQuick";
-import { readOne } from "@grimoire/grimoire.*";
+import { readOne } from "@grimoire";
 
 export default async function Page({ params }: { params: Promise<{ id: string }> }) {
   const { id } = await params;
