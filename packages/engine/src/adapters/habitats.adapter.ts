1c26ef566700f648e0c4b8add5603285479c4fa8
Mario Godin <victor@sheet2scene.ca>
2025-11-24 23:25:17 -0500

    big fix

--- packages/engine/src/adapters/habitats.adapter.ts
+++ packages/engine/src/adapters/habitats.adapter.ts
@@ -1,4 +1,4 @@
-import * as MQ_HABITATS from "../content/fantasy/habitats/index";
+import * as MQ_HABITATS from "../../content/fantasy/habitats/index";
 
 export function getHabitats() {
   return MQ_HABITATS;
--- packages/engine/src/adapters/spells.adapter.ts
+++ packages/engine/src/adapters/spells.adapter.ts
@@ -1,4 +1,4 @@
-import * as MQ_SPELLS from "../content/fantasy/spells/index";
+import * as MQ_SPELLS from "../../content/fantasy/spells/index";
 
 export function getAllSpells() {
   return Object.values(MQ_SPELLS).flat();
