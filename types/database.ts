0da419d42605f4aa8cc6fecdb36ab86a92d180de
Mario Godin <victor@sheet2scene.ca>
2025-11-26 17:13:45 -0500

    fix: clean tsconfig + stable fantasy paths

--- types/database.ts
+++ types/database.ts
@@ -1,4 +1,35 @@
+};
 export type Tables = {
-  worlds: { Row: { id: string; name: string } };
-  characters: { Row: { id: string; name: string; world_id: string } };
+  worlds: {
+    Row: {
+      id: string;
+      name: string;
+    };
+    Insert: {
+      id?: string;
+      name: string;
+    };
+    Update: {
+      id?: string;
+      name?: string;
+    };
+  };
+
+  characters: {
+    Row: {
+      id: string;
+      name: string;
+      world_id: string;
+    };
+    Insert: {
+      id?: string;
+      name: string;
+      world_id: string;
+    };
+    Update: {
+      id?: string;
+      name?: string;
+      world_id?: string;
+    };
+  };
 };
