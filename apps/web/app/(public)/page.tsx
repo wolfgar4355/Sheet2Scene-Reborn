44adbbf293176a436e37b941decd079b7560086f
Mario Godin <victor@sheet2scene.ca>
2025-12-24 21:52:24 -0500

    fix web

--- apps/web/app/(public)/page.tsx
+++ apps/web/app/(public)/page.tsx
@@ -3,7 +3,7 @@
 import { useState, useRef } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { triggerBookAnimation } from "@/components/BookAnimation";
+import { triggerBookAnimation } from "../../components/BookAnimation";
 
 export default function PublicLandingPage() {
   const router = useRouter();
